d81c5f30a0b4961fecc1fa9edde7e31f24ce69b2
itsandromeda <itsandromeda@gmail.com>
2017-04-01 14:31:04 -0500

    Modal images

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-/*global document, select*/
+/*global document, select, alert, console*/
 var img = document.getElementsByClassName("portfolio"),
     i;
 
@@ -6,9 +6,7 @@ select.onchange = function () {
     "use strict";
     if (select.value === "original") {
         for (i = 0; i < img.length; i += 1) {
-            img[i].classList.remove("black-white");
-            img[i].classList.remove("invert");
-            img[i].classList.remove("sepia");
+            img[i].setAttribute("class", "portfolio");
         }
     } else if (select.value === "black-white") {
         for (i = 0; i < img.length; i += 1) {
@@ -24,3 +22,25 @@ select.onchange = function () {
         }
     }
 };
+
+var modal = document.getElementById('modal');
+var modalImg = document.getElementById("modal-pic");
+var captionText = document.getElementById("caption");
+
+var modalDisplay = function () {
+    "use strict";
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+};
+
+for (i = 0; i < img.length; i += 1) {
+    document.getElementById('img0' + i).addEventListener('click', modalDisplay);
+}
+
+var close = document.getElementsByClassName("close")[0];
+
+close.addEventListener('click', function () {
+    "use strict";
+    modal.style.display = "none";
+});
